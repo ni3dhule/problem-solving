27ff6d77326deaaaae0737ddcafa86ca38d43c8b
niteen <ndhule1988@gmail.com>
2023-09-22 00:29:50 +0530

    GeekForGeek : Find a pair with the given sum in an array

---
+++ src/main/java/com/problem/solving/geekforgeek/PairOfElementsTargetSum.java
@@ -0,0 +1,32 @@
+package com.problem.solving.geekforgeek;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PairOfElementsTargetSum {
+    //Find a pair with the given sum in an array
+    public List<String> getPairsOfElementForTargetSum(int []nums, int target) {
+        // sort the given array and check the pair of item with sum = target element
+        Arrays.sort(nums);
+
+        int left = 0;
+        int right = nums.length - 1;
+        String pair;
+        ArrayList<String> result = new ArrayList<>();
+
+        while(left < right) {
+            if(nums[left]+nums[right] == target) {
+                pair = "("+nums[left]+","+nums[right]+")";
+                result.add(pair);
+            }
+            if (nums[left]+nums[right] < target) {
+                left++;
+            } else {
+                right++;
+            }
+        }
+
+        return result;
+    }
+}
