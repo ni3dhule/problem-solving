adb6645079d38e86bdf9db5f29e3b4b4f4cc698f
niteen <ndhule1988@gmail.com>
2023-09-22 23:58:56 +0530

    GeekForGeek : Find a pair with the given sum in an array -- added test case

---
+++ src/test/java/com/problem/solving/geekforgeek/PairOfElementsTargetSumTest.java
@@ -0,0 +1,25 @@
+package com.problem.solving.geekforgeek;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PairOfElementsTargetSumTest {
+    //Find a pair with the given sum in an array
+    @Test
+    public void getPairsOfElementForTargetSumTest() {
+        //public List<String> getPairsOfElementForTargetSum(int []nums, int target) {
+        int []nums = new int[] {2, 4, 1, 6, 8, 9, 7, 3};
+        List<String> actualResult = PairOfElementsTargetSum.getPairsOfElementForTargetSum(nums, 10);
+        ArrayList<String> expectedResult = new ArrayList<>();
+        expectedResult.add("(1,9)");
+        expectedResult.add("(2,8)");
+        expectedResult.add("(3,7)");
+        expectedResult.add("(4,6)");
+
+        Assertions.assertTrue(actualResult.containsAll(expectedResult));
+
+    }
+}
