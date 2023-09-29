6adc73cef690cf955f73c6d0024eb0b96f46acbb
niteen <ndhule1988@gmail.com>
2023-09-30 00:21:50 +0530

    problem solved from leet code [check monotonic array as input] - with test case

---
+++ src/main/java/com/problem/solving/leetcode/MonotonicArray.java
@@ -0,0 +1,30 @@
+package com.problem.solving.leetcode;
+
+public class MonotonicArray {
+    private MonotonicArray(){}
+    public static boolean isMonotonic(int[] inputs) {
+        // Runtime 1 ms
+        if(inputs.length == 1)
+            return true;
+        int firstElement = inputs[0];
+        int lastElement = inputs[inputs.length-1];
+        boolean isAscending = false;
+        if(firstElement < lastElement)
+            isAscending = true;
+        boolean monotonic = true;
+        for (int i = 0; i < inputs.length-1; i++){
+            if(isAscending && inputs[i] <= inputs[i+1]){
+                continue;
+            } else if (isAscending && inputs[i] > inputs[i+1]) {
+                monotonic=false;
+                break;
+            } else if(!isAscending && inputs[i] >= inputs[i+1]) {
+                continue;
+            } else if(!isAscending && inputs[i] < inputs[i+1]) {
+                monotonic=false;
+                break;
+            }
+        }
+        return monotonic;
+    }
+}
