ad075c05d9ae8b6997fb33088189aec2af09da22
niteen <ndhule1988@gmail.com>
2023-10-01 00:17:21 +0530

    problem solved from leet code [check 132 pattern] - with test case

---
+++ src/test/java/com/problem/solving/leetcode/Find132PatternTest.java
@@ -0,0 +1,23 @@
+package com.problem.solving.leetcode;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class Find132PatternTest {
+    @Test
+    public void find132PatternNegativeTestCase(){
+        int[] input = new int[] {1,2,3,4};
+        boolean expectedResult = false;
+        boolean actualResult = Find132Pattern.is132Pattern(input);
+        Assertions.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void find132PatternPositiveTestCase(){
+        int[] input = new int[] {3,1,4,2};
+        boolean expectedResult = true;
+        boolean actualResult = Find132Pattern.is132Pattern(input);
+        Assertions.assertEquals(actualResult, expectedResult);
+    }
+
+}
