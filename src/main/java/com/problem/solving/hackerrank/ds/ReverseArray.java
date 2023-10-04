ec281e19d1d844ad329e951948d61aeb905f666e
niteen <ndhule1988@gmail.com>
2023-10-05 00:26:17 +0530

    problem solved from hackerrank [reverse the 1d array] - with test case

---
+++ src/main/java/com/problem/solving/hackerrank/ds/ReverseArray.java
@@ -0,0 +1,11 @@
+package com.problem.solving.hackerrank.ds;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ReverseArray {
+    public List<Integer> reverseArray(List<Integer> list) {
+        Collections.reverse(list);
+        return list;
+    }
+}
