0606fa573b3459bdbf27fa1afd3b8f4c681dd698
niteen <ndhule1988@gmail.com>
2023-10-08 23:41:01 +0530

    problem solved from leetcode [Find max dot product for given two arrays] - with test case

---
+++ src/test/java/com/problem/solving/leetcode/MaxDotProductTest.java
@@ -0,0 +1,15 @@
+package com.problem.solving.leetcode;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class MaxDotProductTest {
+    @Test
+    public void maxDotProductPositiveTestCase(){
+        int[] num1 = new int[]{2,1,-2,5};
+        int[] num2 = new int[]{3,0,-6};
+        int expectedResult = 18;
+        int actualResult = MaxDotProduct.maxDotProduct(num1, num2);
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+}
