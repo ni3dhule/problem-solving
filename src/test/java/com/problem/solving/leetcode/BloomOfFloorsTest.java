e20a98c5f11144315c82847705a6e40255b46b9b
niteen <ndhule1988@gmail.com>
2023-10-12 21:22:20 +0530

    problem solved from leetcode [find nnumber of floors in a bloom] - with test case

---
+++ src/test/java/com/problem/solving/leetcode/BloomOfFloorsTest.java
@@ -0,0 +1,17 @@
+package com.problem.solving.leetcode;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class BloomOfFloorsTest {
+    @Test
+    public void getBloomOfFloorTestPositiveTestCase() {
+        int[][] flowers = new int[][]{{1,6},{3,7},{9,12},{4,13}};
+        int[] peoples = new int[]{2,3,7,11};
+        int[] expectedResult =new int[]{1,2,2,2};
+
+        int[]  actualResult = BloomOfFloors.fullBloomFlowers(flowers, peoples);
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+
+    }
+}
