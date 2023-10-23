ed9fe0d0b64f5787731ad1042d86446691838e76
niteen <ndhule1988@gmail.com>
2023-10-23 23:32:03 +0530

    problem solved from leetcode [power of four] added test case !

---
+++ src/test/java/com/problem/solving/leetcode/PowerOfFour.java
@@ -0,0 +1,10 @@
+package com.problem.solving.leetcode;
+
+public class PowerOfFour {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        return Math.log(n) / Math.log(4) % 1 == 0;
+    }
+}
