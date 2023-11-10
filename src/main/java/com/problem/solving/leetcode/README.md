60f7395d9349a311246b2cce2c3922983d5b0612
niteen <ndhule1988@gmail.com>
2023-11-11 01:06:31 +0530

    problem solved from leet-code [ restore the array from adjacent arrays ] added test case ! Sonar issue fixes

--- src/main/java/com/problem/solving/leetcode/README.md
+++ src/main/java/com/problem/solving/leetcode/README.md
@@ -40,3 +40,5 @@ https://leetcode.com/problems/eliminate-maximum-number-of-monsters/description/?
 https://leetcode.com/problems/determine-if-a-cell-is-reachable-at-a-given-time/?envType=daily-question&envId=2023-11-08
 20. 1759. Count Number of Homogenous Substrings
 https://leetcode.com/problems/count-number-of-homogenous-substrings/?envType=daily-question&envId=2023-11-09
+21. 1743. Restore the Array From Adjacent Pairs
+https://leetcode.com/problems/restore-the-array-from-adjacent-pairs/description/?envType=daily-question&envId=2023-11-10
---
+++ src/main/java/com/problem/solving/leetcode/RestoreArrayFromAdjacentPairs.java
@@ -0,0 +1,34 @@
+package com.problem.solving.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RestoreArrayFromAdjacentPairs {
+    private RestoreArrayFromAdjacentPairs(){}
+    public static int[] restoreArray(int[][] adjacentPairs) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int[] pair : adjacentPairs) {
+            map.putIfAbsent(pair[0], new ArrayList<>());
+            map.putIfAbsent(pair[1], new ArrayList<>());
+
+            map.get(pair[0]).add(pair[1]);
+            map.get(pair[1]).add(pair[0]);
+        }
+        int[] arr = new int[adjacentPairs.length + 1];
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            if (entry.getValue().size() == 1) {
+                arr[0] = entry.getKey();
+                arr[1] = entry.getValue().get(0);
+                break;
+            }
+        }
+        for (int i=2; i < arr.length; i++) {
+            List<Integer> a = map.get(arr[i-1]);
+            arr[i] = a.get(0) == arr[i-2] ? a.get(1) : a.get(0);
+        }
+        return arr;
+    }
+}
