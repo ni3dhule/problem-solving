38866aa899a469b6345d785fa249f38adb16755e
niteen <ndhule1988@gmail.com>
2023-11-16 17:05:28 +0530

    problem solved from leet-code [palindromic sequence of k ] added test case ! Sonar issue fixes

---
+++ src/main/java/com/problem/solving/leetcode/dailychalange/november/README.md
@@ -0,0 +1,3 @@
+1. 1980. Find Unique Binary String
+https://leetcode.com/problems/find-unique-binary-string/description/?envType=daily-question&envId=2023-11-16
+2. 
---
+++ src/test/java/com/problem/solving/leetcode/dailychalange/november/FindUniqueBinaryStringTest.java
@@ -0,0 +1,14 @@
+package com.problem.solving.leetcode.dailychalange.november;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class FindUniqueBinaryStringTest {
+    @Test
+    public void shouldReturnFindUniqueBinaryString_Success(){
+        String[] input = new String[]{"01","10"};
+        String expectedResult = "00";
+        String actualResult = FindUniqueBinaryString.findDifferentBinaryString(input);
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+}
