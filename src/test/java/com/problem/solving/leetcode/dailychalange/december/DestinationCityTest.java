18158b95e004cd1cc03bd211c2df4ba636868b00
niteen <ndhule1988@gmail.com>
2023-12-15 23:59:50 +0530

    problem solved from leet-code [Destination city ] added test case ! Sonar issue fixes

---
+++ src/test/java/com/problem/solving/leetcode/dailychalange/december/DestinationCityTest.java
@@ -0,0 +1,10 @@
+package com.problem.solving.leetcode.dailychalange.december;
+
+import org.junit.Test;
+
+public class DestinationCityTest {
+    @Test
+    public void shouldReturnDestinationCity_Success()  {
+        String[][] input =  new String[][]{{"London","New York"},{"New York","Lima"},{"Lima","Sao Paulo"}};
+    }
+}
