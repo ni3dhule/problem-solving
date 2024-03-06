fddd35556c35f26b17e12df590962f62a1a74d18
niteen <ndhule1988@gmail.com>
2024-03-06 22:18:13 +0530

    6. 141. Linked List Cycle

--- src/main/java/com/problem/solving/leetcode/dailychalange/year2024/march/LinkedListCycle.java
+++ src/main/java/com/problem/solving/leetcode/dailychalange/year2024/march/LinkedListCycle.java
@@ -1,17 +1,20 @@
 package com.problem.solving.leetcode.dailychalange.year2024.march;
 
 public class LinkedListCycle {
-    private LinkedListCycle(){}
-    public boolean hasCycle(ListNode head) {
-        if(head==null) return false;
+    private LinkedListCycle() {
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
         ListNode fast = head;
         ListNode slow = head;
 
-        while(fast.next!=null && fast.next.next!=null)
-        {
-            fast=fast.next.next;
-            slow=slow.next;
-            if(fast==slow) return true;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow)
+                return true;
         }
         return false;
     }
--- src/main/java/com/problem/solving/leetcode/dailychalange/year2024/march/README.md
+++ src/main/java/com/problem/solving/leetcode/dailychalange/year2024/march/README.md
@@ -10,3 +10,4 @@ https://leetcode.com/problems/bag-of-tokens/description/?envType=daily-question&
 https://leetcode.com/problems/minimum-length-of-string-after-deleting-similar-ends/description/?envType=daily-question&envId=2024-03-05
 6. 141. Linked List Cycle
 https://leetcode.com/problems/linked-list-cycle/description/?envType=daily-question&envId=2024-03-06
+7. 
