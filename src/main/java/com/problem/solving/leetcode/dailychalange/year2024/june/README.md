ddcbfc4dec4717dbd40eb592d52000034d122211
niteen <ndhule1988@gmail.com>
2024-06-02 16:40:28 +0530

    2. 344. Reverse String

--- src/main/java/com/problem/solving/leetcode/dailychalange/year2024/june/README.md
+++ src/main/java/com/problem/solving/leetcode/dailychalange/year2024/june/README.md
@@ -1,2 +1,4 @@
 1. 3110. Score of a String
 https://leetcode.com/problems/score-of-a-string/description/?envType=daily-question&envId=2024-06-01
+2. 344. Reverse String
+https://leetcode.com/problems/reverse-string/description/?envType=daily-question&envId=2024-06-02
---
+++ src/main/java/com/problem/solving/leetcode/dailychalange/year2024/june/ReverseString.java
@@ -0,0 +1,11 @@
+package com.problem.solving.leetcode.dailychalange.year2024.june;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
+    }
+}
