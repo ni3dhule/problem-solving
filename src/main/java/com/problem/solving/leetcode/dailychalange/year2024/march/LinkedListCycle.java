e62dbceb16683b087f525bb2fdea0c087347a30d
niteen <ndhule1988@gmail.com>
2024-06-03 22:14:21 +0530

    6. 141. Linked List Cycle

---
+++ src/main/java/com/problem/solving/leetcode/dailychalange/year2024/march/LinkedListCycle.java
@@ -0,0 +1,18 @@
+package com.problem.solving.leetcode.dailychalange.year2024.march;
+
+public class LinkedListCycle {
+    private LinkedListCycle(){}
+    public boolean hasCycle(ListNode head) {
+        if(head==null) return false;
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast.next!=null && fast.next.next!=null)
+        {
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow) return true;
+        }
+        return false;
+    }
+}
