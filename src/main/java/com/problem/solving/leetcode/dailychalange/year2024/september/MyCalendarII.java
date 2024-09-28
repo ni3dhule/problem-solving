37ad6215307b469c2cc45d1521f9cde783468e34
niteen <ndhule1988@gmail.com>
2024-09-28 23:47:20 +0530

    28. 641. Design Circular Deque

--- src/main/java/com/problem/solving/leetcode/dailychalange/year2024/september/MyCalendarII.java
+++ src/main/java/com/problem/solving/leetcode/dailychalange/year2024/september/MyCalendarII.java
@@ -3,12 +3,11 @@ package com.problem.solving.leetcode.dailychalange.year2024.september;
 import java.util.ArrayList;
 import java.util.List;
 
-class Event
-{
+class Event {
     int start;
     int end;
-    public Event(int start, int end)
-    {
+
+    public Event(int start, int end) {
         this.start = start;
         this.end = end;
     }
@@ -24,17 +23,13 @@ class MyCalendarII {
     }
 
     public boolean book(int start, int end) {
-
-        for(Event e : overLapBookings)
-        {
-            if(isOverlap(e, start, end))
+        for (Event e : overLapBookings) {
+            if (isOverlap(e, start, end))
                 return false;
         }
 
-        for(Event e : bookings)
-        {
-            if(isOverlap(e, start, end))
-            {
+        for (Event e : bookings) {
+            if (isOverlap(e, start, end)) {
                 overLapBookings.add(getOverlapEvent(e, start, end));
             }
         }
@@ -43,13 +38,11 @@ class MyCalendarII {
         return true;
     }
 
-    public boolean isOverlap(Event e, int start, int end)
-    {
-        return Math.max(e.start, start)<Math.min(e.end, end);
+    public boolean isOverlap(Event e, int start, int end) {
+        return Math.max(e.start, start) < Math.min(e.end, end);
     }
 
-    public Event getOverlapEvent(Event e, int start, int end)
-    {
+    public Event getOverlapEvent(Event e, int start, int end) {
         return new Event(Math.max(e.start, start), Math.min(e.end, end));
     }
 }
